1b0b037bedfe2a229e9cfeaa782033dbd99e141b
michten <50970761+michten@users.noreply.github.com>
2019-10-15 13:52:06 +0200

    Update vim-conf.sh
    Added more github repos, cleanups

--- vim-conf.sh
+++ vim-conf.sh
@@ -2,6 +2,8 @@
 #
 # Vim configuration and plugins installation, specially vim-orgmode and suggested plugins.
 # For user only. Need curl and 7z, no git dependence.
+# Plugins are grouped in similar installation type.
+# Vim configuration ia at the end of the script.
 
 # No git, download repo in zip, 7z -o doesn't work with ~
 user_name=$(whoami)
@@ -12,21 +14,55 @@ my_colors="${user_path}/.vim/colors"
 my_plugins_path="${user_path}/.vim/my-plugins-vim-git"
 
 
+#TODO: warning/prompt if .vim and .vimrc exists
+
+
 rm -r "${my_plugins_path}"
-mkdir -p "${pathogen_autoload_path}" "${pathogen_bundle_path}" "${my_colors}" "${my_plugins_path}" &&
+mkdir -p "${pathogen_autoload_path}" "${pathogen_bundle_path}" "${my_colors}" "${my_plugins_path}"
 
 
+#### Type one
 # install/update pathogen
-curl -LSo "${pathogen_autoload_path}/pathogen.vim" 'https://tpo.pe/pathogen.vim'
+curl -LSso "${pathogen_autoload_path}/pathogen.vim" 'https://tpo.pe/pathogen.vim'
 
 # install/update vim-sensible
-curl -LSo "${pathogen_bundle_path}/sensible.vim" 'https://raw.githubusercontent.com/tpope/vim-sensible/master/plugin/sensible.vim'
+curl -LSso "${pathogen_bundle_path}/sensible.vim" 'https://raw.githubusercontent.com/tpope/vim-sensible/master/plugin/sensible.vim'
 
 # my colortheme
-curl -LSo "${my_colors}/herald_mod.vim" 'https://raw.githubusercontent.com/michten/buster-inst/master/07_herald_mod.vim'
+curl -LSso "${my_colors}/herald_mod.vim" 'https://raw.githubusercontent.com/michten/buster-inst/master/07_herald_mod.vim'
+####
 
 
-# My ~/.vimrc config file
+#### Type two
+# Links for github repos of vim plugins
+# add/delete repository zip url here
+vim_git_repos=(
+'https://github.com/dense-analysis/ale/archive/master.zip'
+'https://github.com/jceb/vim-orgmode/archive/master.zip'
+'https://github.com/tpope/vim-speeddating/archive/master.zip'
+'https://github.com/tpope/vim-repeat/archive/master.zip'
+'https://github.com/vim-scripts/utl.vim/archive/master.zip'
+'https://github.com/majutsushi/tagbar/archive/master.zip'
+'https://github.com/chrisbra/NrrwRgn/archive/master.zip'
+'https://github.com/mattn/calendar-vim/archive/master.zip'
+)
+#'https://github.com/artemkin/taglist.vim/archive/master.zip'
+#'https://github.com/inkarkat/vim-SyntaxRange/archive/master.zip'
+
+install_repo() {
+	local link_to_github=$1
+	curl -LSso "${my_plugins_path}/master.zip" "$link_to_github" &&
+	7z x "${my_plugins_path}/master.zip" -o"${my_plugins_path}" &&
+	rm -- "${my_plugins_path}/master.zip" && true
+}
+for repo in ${vim_git_repos[@]}; do
+	install_repo "$repo" && true
+done
+####
+
+
+#### Configuration
+# ~/.vimrc config file
 echo \
 "syntax on
 filetype plugin indent on
@@ -39,25 +75,4 @@ inoremap kk <ESC>
 colorscheme herald_mod
 
 execute pathogen#infect('bundle/{}', 'my-plugins-vim-git/{}')" > "${user_path}/.vimrc"
-
-
-# linki for github repos of vim plugins
-# add another repository here
-vim_git_repos=(
-'https://github.com/jceb/vim-orgmode/archive/master.zip'
-'https://github.com/tpope/vim-speeddating/archive/master.zip'
-'https://github.com/dense-analysis/ale/archive/master.zip'
-)
-
-
-install_repo() {
-	link=$1
-	curl -LSo "${my_plugins_path}/master.zip" "$link" &&
-	7z x "${my_plugins_path}/master.zip" -o"${my_plugins_path}" &&
-	rm -- "${my_plugins_path}/master.zip" && true
-}
-
-
-for repo in ${vim_git_repos[@]}; do
-	install_repo "$repo" && true
-done
+####
